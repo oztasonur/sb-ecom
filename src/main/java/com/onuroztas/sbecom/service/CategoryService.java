2fb8a2cd0720a72d3e0353b305599744909ca30d
Onur Oztas <github@onuroztas.com>
2025-06-14 11:18:09 +0300

    Challenge: Update Category

--- src/main/java/com/onuroztas/sbecom/service/CategoryService.java
+++ src/main/java/com/onuroztas/sbecom/service/CategoryService.java
@@ -11,4 +11,6 @@ public interface CategoryService {
     List<Category> getAllCategories();
     void createCategory(Category category);
     String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
 }
