3d95e42cc1d1de8619585550740619828b0a51fa
Onur Oztas <github@onuroztas.com>
2025-06-14 06:21:10 +0300

    Initial commit

---
+++ src/test/java/com/onuroztas/sbecom/SbEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuroztas.sbecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
