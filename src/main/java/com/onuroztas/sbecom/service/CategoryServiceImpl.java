ff34a74e8fde75ac9d021ff74a2979c4bb51abfc
Onur Oztas <github@onuroztas.com>
2025-07-06 09:45:57 +0300

    Making Category Persistent

--- src/main/java/com/onuroztas/sbecom/service/CategoryServiceImpl.java
+++ src/main/java/com/onuroztas/sbecom/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.onuroztas.sbecom.service;
 
 import com.onuroztas.sbecom.model.Category;
+import com.onuroztas.sbecom.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -16,32 +18,40 @@ public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
     private Long nextId = 1L;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+
+        List<Category> categories = categoryRepository.findAll();
+
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with categoryId: " + categoryId + " deleted successfully.";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+
+        List<Category> categories = categoryRepository.findAll();
+
         Optional<Category> optionalCategory = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst();
@@ -49,7 +59,8 @@ public class CategoryServiceImpl implements CategoryService{
         if(optionalCategory.isPresent()) {
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            Category savedCategory = categoryRepository.save(existingCategory);
+            return savedCategory;
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
         }
