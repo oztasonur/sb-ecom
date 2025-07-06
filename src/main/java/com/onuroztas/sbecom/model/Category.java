423ddfb054b1fe6c1604cdc0a5b35f61599ad1d2
Onur Oztas <github@onuroztas.com>
2025-07-06 09:29:35 +0300

    Add CategoryRepository

--- src/main/java/com/onuroztas/sbecom/model/Category.java
+++ src/main/java/com/onuroztas/sbecom/model/Category.java
@@ -2,14 +2,20 @@ package com.onuroztas.sbecom.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
+
+    public Category() {
+    }
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
