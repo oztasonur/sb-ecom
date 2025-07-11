a3df54938dbba0a9c78a4d8260622f8a8ccec19c
Onur Oztas <github@onuroztas.com>
2025-07-11 12:57:46 +0300

    Adding More Validations

--- src/main/java/com/onuroztas/sbecom/model/Category.java
+++ src/main/java/com/onuroztas/sbecom/model/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +22,6 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
+    @Size(min = 5, message = "Category name must contain at leas 5 characters")
     private String categoryName;
     }
