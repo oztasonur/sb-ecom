f0d55373ed332ef6e93e43658137bd55e5bc5266
Onur Oztas <github@onuroztas.com>
2025-07-11 17:05:09 +0300

    Challenge: Implementing DTOs For DeleteCategory

--- src/main/java/com/onuroztas/sbecom/controller/CategoryController.java
+++ src/main/java/com/onuroztas/sbecom/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.onuroztas.sbecom.controller;
 
 
 import com.onuroztas.sbecom.model.Category;
+import com.onuroztas.sbecom.payload.CategoryDTO;
 import com.onuroztas.sbecom.payload.CategoryResponse;
 import com.onuroztas.sbecom.service.CategoryService;
 import jakarta.validation.Valid;
@@ -34,22 +35,22 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
-        categoryService.createCategory(category);
-        return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId)  {
-            String status = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId)  {
+            CategoryDTO deletedCategoryDTO = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(deletedCategoryDTO, HttpStatus.OK);
     }
 
     @PutMapping("/public/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+    public ResponseEntity<CategoryDTO> updateCategory(@RequestBody CategoryDTO categoryDTO,
                                                  @PathVariable Long categoryId) {
-            Category savedCategory = categoryService.updateCategory(category, categoryId);
-            return new ResponseEntity<>("Category with category id: " + categoryId, HttpStatus.OK);
+            CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
+            return new ResponseEntity<>(savedCategoryDTO, HttpStatus.OK);
     }
 
 
--- src/main/java/com/onuroztas/sbecom/service/CategoryService.java
+++ src/main/java/com/onuroztas/sbecom/service/CategoryService.java
@@ -3,16 +3,13 @@ package com.onuroztas.sbecom.service;
 import com.onuroztas.sbecom.model.Category;
 import com.onuroztas.sbecom.payload.CategoryDTO;
 import com.onuroztas.sbecom.payload.CategoryResponse;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 
 public interface CategoryService {
 
     CategoryResponse getAllCategories();
-    void createCategory(Category category);
-    String deleteCategory(Long categoryId);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+    CategoryDTO deleteCategory(Long categoryId);
 
-    Category updateCategory(Category category, Long categoryId);
+    CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
 }
